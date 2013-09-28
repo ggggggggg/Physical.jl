7dd71d3b3594edd620436a17b831e4de6b58b488
ggggggggg <galenoneil@Galen-ONeils-MacBook-Air.local>
2013-09-27 21:20:17 -0600

    Uncertain no longer subtype of FloatingPoint
    It makes everything simpler

--- src/Uncertainty.jl
+++ src/Uncertainty.jl
@@ -1,9 +1,9 @@
 module Uncertainty
 
 # adding methods to
-import Base: promote_rule, convert, show, sqrt, +, *, -, /, ^, .*, ./, .^, ==, log, log10, exp
+import Base: promote_rule, convert, show, sqrt, +, *, -, /, ^, .*, ./, .^, ==, log, log10, exp, isapprox
 
-type Uncertain{T<:FloatingPoint} <: FloatingPoint
+type Uncertain{T<:FloatingPoint}
     v::T # value
     u::T # uncertainty standard deviation
 end
@@ -12,13 +12,22 @@ correlation(x::Uncertain,y::Uncertain) = 0.0
 covariance(x::Uncertain,y::Uncertain)  = 0.0
 Uncertain(x::Number, y::Number) = Uncertain(convert(FloatingPoint, x), convert(FloatingPoint, y))
 Uncertain(x::FloatingPoint, y::Number) = Uncertain(x, convert(FloatingPoint, y))
+Uncertain(x::Number) = Uncertain(x, convert(typeof(x), 0))
 
 quadsum_neg(x::Number...) = sqrt(sum(sign([x...]).*([x...].^2)))
 +(x::Uncertain, y::Uncertain) = Uncertain_(x.v+y.v, quadsum_neg(x.u,+y.u,covariance(x,y)))
++(x::Uncertain, y::Number) = x+Uncertain(y)
++(x::Number, y::Uncertain) = Uncertain(x)+y
 -(x::Uncertain) = Uncertain(-x.v, x.u)
 -(x::Uncertain, y::Uncertain) = x + -y
+-(x::Uncertain, y::Number) = x-Uncertain(y)
+-(x::Number, y::Uncertain) = Uncertain(x)-y
 *(x::Uncertain, y::Uncertain) = (v = x.v*y.v; Uncertain_(v, v*quadsum_neg(x.u/x.v, y.u/y.v, 2*x.u*y.u*correlation(x,y)/(x.v*y.v))))
+*(x::Uncertain, y::Number) = x*Uncertain(y)
+*(x::Number, y::Uncertain) = Uncertain(x)*y
 /(x::Uncertain, y::Uncertain) = (v = x.v/y.v; Uncertain_(v, v*quadsum_neg(x.u/x.v, y.u/y.v, -2*x.u*y.u*correlation(x,y)/(x.v*y.v))))
+/(x::Uncertain, y::Number) = x/Uncertain(y)
+/(x::Number, y::Uncertain) = Uncertain(x)/y
 log(x::Uncertain) = (v = log(x.v); Uncertain_(v, x.u/x.v))
 log(base::Number, x::Uncertain) = (v = log(base,x.v); Uncertain_(v, x.u/(x.v*log(base))))
 log10(x::Uncertain) = log(10, x)
@@ -31,37 +40,22 @@ log10(x::Uncertain) = log(10, x)
 ^(x::Number, y::Uncertain) = (v = x^y.v; Uncertain_(v, abs(v*log(x)*y.u)))
 exp(x::Uncertain) = (v = exp(x.v); Uncertain_(v, abs(v*x.u)))
 ^{T<:Number}(x::MathConst{:e},y::Uncertain{T}) = exp(y)
-==(x::Uncertain, y::Uncertain) = isapprox(x.v,y.v) && isapprox(x.u,y.u)
+==(x::Uncertain, y::Uncertain) = ==(x.v,y.v) && ==(x.u,y.u)
+isapprox(x::Uncertain, y::Uncertain) = isapprox(x.v, y.v) && isapprox(x.u, y.u)
 
-promote_rule{S<:FloatingPoint}(::Type{Bool}, ::Type{Uncertain{S}}) = Uncertain{S}
-promote_rule{T<:Complex, S<:FloatingPoint}(::Type{T}, ::Type{Uncertain{S}}) = Uncertain{S}
-promote_rule{S<:FloatingPoint}(::Type{BigFloat}, ::Type{Uncertain{S}}) = Uncertain{BigFloat}
-promote_rule{S<:FloatingPoint}(::Type{BigInt}, ::Type{Uncertain{S}}) = Uncertain{BigFloat}
-promote_rule{T, S<:FloatingPoint}(::Type{MathConst{T}}, ::Type{Uncertain{S}}) = Uncertain{S}
-promote_rule{S<:FloatingPoint}(::Type{Rational}, ::Type{Uncertain{S}}) = Uncertain{promote_type(T,S)}
-promote_rule{T<:Real, S<:FloatingPoint}(::Type{T}, ::Type{Uncertain{S}}) = Uncertain{promote_type(T,S)}
 promote_rule{T<:FloatingPoint, S<:FloatingPoint}(::Type{Uncertain{T}}, ::Type{Uncertain{S}}) = Uncertain{promote_type(T,S)}
-promote_rule{T<:FloatingPoint}(::Type{T}, ::Type{Uncertain{T}}) = Uncertain{T}
 
 convert{T<:FloatingPoint, S<:FloatingPoint}(::Type{Uncertain{T}}, x::Uncertain{S}) = Uncertain(convert(T,x.v), convert(T,x.u))
-convert{T<:FloatingPoint,s}(::Type{Uncertain{T}},x::MathConst{s}) = Uncertain(convert(T, eval(s)), convert(T,0))
-convert{T<:FloatingPoint, S<:Complex}(::Type{Uncertain{T}},x::S) = error("Uncertain{Complex} not supported")
-convert{T<:FloatingPoint, S<:Rational}(::Type{Uncertain{T}},x::S) = Uncertain(convert(T,x),convert(T,0))
-convert{T<:FloatingPoint, S<:Number}(::Type{Uncertain{T}},x::S) = Uncertain(convert(T,x),convert(T,0))
-convert{T<:FloatingPoint, S<:Real}(::Type{Uncertain{T}},x::S) = Uncertain(convert(T,x),convert(T,0))
-convert{T<:FloatingPoint, S<:Integer}(::Type{Uncertain{T}},x::S) = Uncertain(convert(T,x),convert(T,0))
-convert{T<:FloatingPoint}(::Type{Uncertain{T}}, x::T) = Uncertain(convert(T,x),convert(T,0))
-convert{T<:FloatingPoint}(::Type{Uncertain{T}},x::T) = Uncertain(convert(T,x),convert(T,0))
-convert{T<:FloatingPoint, S<:Number}(::Type{Uncertain{T}}, x::S) = Uncertain(convert(T,x), convert(T,0))
-convert{T<:FloatingPoint}(::Type{Uncertain}, x::T) = Uncertain(x, convert(T,0))
-convert(::Type{Uncertain}, x::Uncertain) = Uncertain(x.v, x.u)
-convert(::Type{Uncertain{BigFloat}}, x::Uncertain) = Uncertain(convert(BigFloat, x.v), convert(BigFloat,x.u))
+#convert(::Type{Uncertain{BigFloat}}, x::Uncertain) = Uncertain(convert(BigFloat, x.v), convert(BigFloat,x.u))
 
 function show(io::IO, x::Uncertain)
     show(io, x.v)
-    print(io, " \u00b1 ")
+    print(io, " ± ")
     show(io, x.u)
 end
+n_signif(x::Uncertain) = iceil(log10(x.v/x.u))
+# because of the macro @sprintf its hard to do dynamic precision strings https://groups.google.com/forum/#!msg/julia-dev/P50UdTGKkRI/KCPnQ6LqH44J
+
 
 export Uncertain
 end
--- test/testUncertain.jl
+++ test/testUncertain.jl
@@ -4,18 +4,18 @@ b = Uncertain(4.1,0.3)
 
 # these all have covariance = 0
 #used http://laffers.net/blog/2010/11/15/error-propagation-calculator/ to calculate test cases
-@assert a+b == Uncertain(6.6,0.5)
-@assert a-b == Uncertain(-1.6, 0.5)
-@assert a*b == Uncertain(10.25, 1.8033579788827288)
-@assert a/b == Uncertain(0.6097560975609757, 0.1072788803618518)
-@assert log(b) == Uncertain(1.410986973710262, 0.07317073170731708)
-@assert log10(b) == Uncertain(0.6127838567197355, 0.03177764501731111)
-@assert exp(b) == Uncertain(60.34028759736194, 18.10208627920858)
-@assert a^b.v == Uncertain(42.81086821817254,28.083929551121184)
-@assert exp(b) == e^b
-@assert 1.0/b == Uncertain(0.24390243902439027, 0.017846519928613924)
-@assert a^-1.0 == Uncertain(0.4,0.064)
-@assert 1*b == b
+@assert isapprox(a+b,Uncertain(6.6,0.5))
+@assert isapprox(a-b, Uncertain(-1.6, 0.5))
+@assert isapprox(a*b,Uncertain(10.25, 1.8033579788827288))
+@assert isapprox(a/b,Uncertain(0.6097560975609757, 0.1072788803618518))
+@assert isapprox(log(b),Uncertain(1.410986973710262, 0.07317073170731708))
+@assert isapprox(log10(b), Uncertain(0.6127838567197355, 0.03177764501731111))
+@assert isapprox(exp(b),Uncertain(60.34028759736194, 18.10208627920858))
+@assert isapprox(a^b.v,Uncertain(42.81086821817254,28.083929551121184))
+@assert isapprox(exp(b),e^b)
+@assert isapprox(1.0/b,Uncertain(0.24390243902439027, 0.017846519928613924))
+@assert isapprox(a^-1.0,Uncertain(0.4,0.064))
+@assert isapprox(1*b,b)
 
 
 # BigFloat
@@ -27,3 +27,8 @@ f = (c+c)/2-c
 f^-1;
 1^d;
 
+@assert isapprox(4*a, a*4)
+@assert isapprox(4+a, a+4)
+@assert isapprox(4-b, -b+4)
+@assert isapprox(4-b, -(b-4))
+@assert isapprox(4/b, 1/(b/4))

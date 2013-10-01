69f8a72f21f8bba5531d0eeb85bb0ac254efc9e3
ggggggggg <galenoneil@Galen-ONeils-MacBook-Air.local>
2013-09-30 21:14:17 -0600

    better printing of Uncertain

--- src/Uncertainty.jl
+++ src/Uncertainty.jl
@@ -1,9 +1,9 @@
 module Uncertainty
 
 # adding methods to
-import Base: promote_rule, convert, show, sqrt, +, *, -, /, ^, .*, ./, .^, ==, log, log10, exp, isapprox
+import Base: promote_rule, convert, show, sqrt, +, *, -, /, ^, .*, ./, .^, ==, log, log10, exp, isapprox, signif
 
-type Uncertain{T<:FloatingPoint}
+type Uncertain{T<:Number}
     v::T # value
     u::T # uncertainty standard deviation
 end
@@ -11,7 +11,6 @@ Uncertain_(v,u) = u >= 0 ? Uncertain(v,u) : error("Uncertain(v=$v, u=$u) u>=0 re
 correlation(x::Uncertain,y::Uncertain) = 0.0
 covariance(x::Uncertain,y::Uncertain)  = 0.0
 Uncertain(x::Number, y::Number) = Uncertain(convert(FloatingPoint, x), convert(FloatingPoint, y))
-Uncertain(x::FloatingPoint, y::Number) = Uncertain(x, convert(FloatingPoint, y))
 Uncertain(x::Number) = Uncertain(x, convert(typeof(x), 0))
 
 quadsum_neg(x::Number...) = sqrt(sum(sign([x...]).*([x...].^2)))
@@ -49,12 +48,36 @@ convert{T<:FloatingPoint, S<:FloatingPoint}(::Type{Uncertain{T}}, x::Uncertain{S
 #convert(::Type{Uncertain{BigFloat}}, x::Uncertain) = Uncertain(convert(BigFloat, x.v), convert(BigFloat,x.u))
 
 function show(io::IO, x::Uncertain)
-    xv_signif = sprintf_signif(x)
-    print(io, xv_signif)
+	v, u = sprintf_signif(x)
+    print(io, v)
     print(io, " ± ")
-    show(io, x.u)
+    print(io, u)
 end
-sprintf_signif(x::Uncertain) = sprintf("%0.$(iceil(log10(x.v/x.u)))f", x.v)
+n_signif(x::Uncertain) = iceil(log10(x.v/x.u))
+signif(x::Uncertain) = Uncertain(signif(x.v,n_signif(x)),x.u)
+
+
+function sprintf_signif(x::Uncertain) 
+	n = n_signif(x)
+	n <= 1 ? (return repr(x.v), repr(x.u)) : 0
+	after_dec = iceil(-log10(x.u))
+	if after_dec <= 0
+		v = sprintf("%d", signif(x.v,n))
+	elseif after_dec >= -3
+		v = sprintf("%0.$(after_dec)f", x.v)
+	else
+		v = sprintf("%.$(n-1)e",x.v)
+	end
+	after_dec = iceil(1-log10(x.u))
+	if after_dec <= 0
+		u = sprintf("%d", signif(x.u,2))
+	elseif after_dec <= 3
+		u = sprintf("%0.$(after_dec)f", x.u)
+	else
+		u = sprintf("%.1e", x.u)
+	end
+	return v,u
+end
 sprintf(fmt::String,args...) = @eval @sprintf($fmt,$(args...))
 # because of the macro @sprintf its hard to do dynamic precision strings https://groups.google.com/forum/#!msg/julia-dev/P50UdTGKkRI/KCPnQ6LqH44J
 # more discussion: https://github.com/JuliaLang/julia/issues/4248#issuecomment-24196846

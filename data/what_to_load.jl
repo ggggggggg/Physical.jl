02296306b1dcffdb123cd7e0a658bac7a1a3d131
ggggggggg <galenoneil@Galen-ONeils-MacBook-Air.local>
2013-10-05 20:40:40 -0600

    added comment documentation to what_to_load.jl

--- data/what_to_load.jl
+++ data/what_to_load.jl
@@ -1,1 +1,8 @@
 load_unit_system("default")
+#load_unit_system("cgs")
+
+
+# to use cgs instead of default, just comment out line 1 and uncomment line 2
+# other systems can be added easily, just make a new folder, fill it with 
+# .jl files that define your prefered system and constants, then add
+# load_unit_system("FOLDER_NAME") to this file and comment out everything else
